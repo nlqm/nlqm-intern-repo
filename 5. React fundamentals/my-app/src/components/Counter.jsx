4a0fbc7f88838e824ef56185329cf64dc685dd37
nlqm <maiqnguyenluu41101@gmail.com>
2025-03-03 07:25:24 +1100

    Update answers and components for task Introduction to Redux Toolkit #24 in redux_fundamentals.md file

--- 5. React fundamentals/my-app/src/components/Counter.jsx
+++ 5. React fundamentals/my-app/src/components/Counter.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement, reset } from "../redux/counterSlice"
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div>
         <h2>This is the Count components: {count}</h2>
-        <button className='mt-4 text-white bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' onClick={() => setCount(count + 1)}>Count up!</button>
+        <Button onClick={() => dispatch(increment())} title="Increase + " />
+        <Button onClick={() => dispatch(decrement())} title="Decrease - " />
+        <Button onClick={() => dispatch(reset())} title="Reset - " />
     </div>
   )
 }
